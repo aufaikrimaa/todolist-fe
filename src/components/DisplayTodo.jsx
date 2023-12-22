442616a98aab871b01bacaa591be382f2191a5af
Aufa Ikrimah <aufaikrimah@gmail.com>
2023-12-22 22:49:33 +0700

    fade-in transition for adding todo

--- src/components/DisplayTodo.jsx
+++ src/components/DisplayTodo.jsx
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { deleteTodo, toggleTodo, editTodo } from '../redux/slice/todoSlice';
 import edit from '../assets/img/edit.svg';
 import remove from '../assets/img/delete.svg';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 const DisplayTodo = () => {
     const [filter, setFilter] = useState('all');
@@ -59,43 +60,47 @@ const DisplayTodo = () => {
             </div>
             <div className="flex items-center justify-center">
                 <ul className='w-1/5'>
-                    {filteredTodos.map((todo) => (
-                        <li key={todo.id} className='flex items-center justify-between w-full border-2 pl-4 pr-2 py-2 my-2'>
-                            <div className="flex items-center space-x-4 w-full">
-                                <input
-                                    type="checkbox"
-                                    checked={todo.completed}
-                                    onChange={() => handleToggleTodo(todo.id)}
-                                />
-                                <span
-                                    style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
-                                    className='w-3/4'
-                                >
-                                    {todo.text}
-                                </span>
-                                <div className="flex items-center space-x-4 justify-end">
-                                    <button
-                                        onClick={() => {
-                                            const newText = prompt('Enter new text:', todo.text);
-                                            if (newText !== null) {
-                                                handleEditTodo(todo.id, newText);
-                                            }
-                                        }}
-                                        className="transform hover:scale-125 transition-transform duration-300"
-                                    >
-                                        <img src={edit} alt="edit" className="w-6" />
-                                    </button>
-                                    <button
-                                        onClick={() => handleDeleteTodo(todo.id)}
-                                        className="transform hover:scale-125 transition-transform duration-300"
-                                    >
-                                        <img src={remove} alt="delete" className="w-7" />
-                                    </button>
-                                </div>
+                    <TransitionGroup>
+                        {filteredTodos.map((todo) => (
+                            <CSSTransition key={todo.id} timeout={500} classNames="fadeIn">
+                                <li key={todo.id} className='flex items-center justify-between w-full border-2 pl-4 pr-2 py-2 my-2'>
+                                    <div className="flex items-center space-x-4 w-full">
+                                        <input
+                                            type="checkbox"
+                                            checked={todo.completed}
+                                            onChange={() => handleToggleTodo(todo.id)}
+                                        />
+                                        <span
+                                            style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
+                                            className='w-3/4'
+                                        >
+                                            {todo.text}
+                                        </span>
+                                        <div className="flex items-center space-x-4 justify-end">
+                                            <button
+                                                onClick={() => {
+                                                    const newText = prompt('Enter new text:', todo.text);
+                                                    if (newText !== null) {
+                                                        handleEditTodo(todo.id, newText);
+                                                    }
+                                                }}
+                                                className="transform hover:scale-125 transition-transform duration-300"
+                                            >
+                                                <img src={edit} alt="edit" className="w-6" />
+                                            </button>
+                                            <button
+                                                onClick={() => handleDeleteTodo(todo.id)}
+                                                className="transform hover:scale-125 transition-transform duration-300"
+                                            >
+                                                <img src={remove} alt="delete" className="w-7" />
+                                            </button>
+                                        </div>
 
-                            </div>
-                        </li>
-                    ))}
+                                    </div>
+                                </li>
+                            </CSSTransition>
+                        ))}
+                    </TransitionGroup>
                 </ul>
             </div>
 
