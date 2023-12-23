7549d0c944070b39f2c9ce6319fe5840ef195e99
Aufa Ikrimah <aufaikrimah@gmail.com>
2023-12-23 09:20:49 +0700

    implement switch mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,35 @@
+import React, { useState } from 'react'
 import InputTodo from "./components/InputTodo"
 import BackgroundIcons from "./components/BackgroundIcons"
 import DisplayTodo from "./components/DisplayTodo"
 import todo from './assets/img/todolist.svg'
+import moon from './assets/img/moon.svg'
+import sun from './assets/img/sun.svg'
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
 
   return (
     <>
-      <div>
-        <BackgroundIcons></BackgroundIcons>
-        <div className="todos xl:mt-30">
-          <h1 className="text-4xl text-center font-bold mb-10 flex items-center justify-center">
-            Plan for today <img src={todo} className="w-15" />
-          </h1>
-          <InputTodo />
-          <DisplayTodo />
+      <div className={`app ${isDarkMode ? 'dark' : 'light'}`}>
+        <div>
+          <BackgroundIcons></BackgroundIcons>
+          <button onClick={toggleDarkMode} className='absolute p-0.5 flex justify-end top-5 right-5 rounded-full'>
+            {isDarkMode ? <img src={moon} alt='dark' className='w-8 h-8' /> : <img src={sun} alt='light' className='w-8 h-8' />}
+          </button>
+          <div className="todos">
+            <h1 className="text-4xl text-center font-bold mb-10 pt-30 flex items-center justify-center">
+              Plan for today <img src={todo} className="w-15" />
+            </h1>
+            <InputTodo />
+            <DisplayTodo />
+          </div>
         </div>
+
       </div>
 
     </>
